2280f04378718a220ae517958160445087e3faf5
Adam Goldberg <goldberg7@wisc.edu>
2016-12-21 16:08:31 -0600

    updated all queue functions

--- queue.c
+++ queue.c
@@ -10,15 +10,15 @@ int initialize(Queue* queue){
 	}
 
 	queue -> size = 0;
-	queue -> front = NULL;
-	queue -> rear = NULL;
+	queue -> head = NULL;
+	queue -> tail = NULL;
 
 	printf("Queue initialized...\n");
 	return 0;
 
 }
 
-int add(Queue* queue, void* data){
+int enqueue(Queue* queue, void* data){
 	
 	if(NULL == queue){
 		printf("Cannot add to unitialized queue...\n");
@@ -26,10 +26,72 @@ int add(Queue* queue, void* data){
 	}
 
 	Queue_Item* item = malloc(sizeof(Queue_Item));
-	
-	//Case where adding to empty queue
+	item -> data = data;
+
+	//empty queue, put first node at head of linked list
 	if(queue -> size == 0){
-		
+		item -> next = queue -> head;
+		queue -> head = item;
+		queue -> tail = item;
+
+		queue -> size++;
+
+		printf("Successfully queued...\n");
+		return 0;
 	}
 	
+	//Add to end of the linked list
+	item -> next = NULL;
+	queue -> tail -> next = item;
+	queue -> tail = item;
+	queue -> size++;
+
+	printf("Successfully queued...\n");
+	return 0;
+	
+	
 }
+
+
+void* dequeue(Queue* queue){
+
+
+	if(NULL == queue || queue -> size == 0){
+		printf("Cannot dequeue an empty queue...\n");
+		return NULL;
+	}
+
+	void* data = queue -> head -> data;
+	Queue_Item* item = queue -> head -> next;
+	free(queue -> head);
+	queue -> head = item;
+	queue -> num_items--;
+
+	return data;
+
+}
+
+
+void* peek(Queue* queue){
+
+	if(NULL == queue || queue -> size == 0){
+		printf("Cannot peek at empty queue...\n");
+		return NULL;
+	}
+
+	return queue -> head -> data;
+
+}
+
+int size(Queue* queue){
+
+	if(NULL == queue){
+		printf("No size of uninitialized queue...\n");
+		return -1;
+	}
+
+	return queue -> size;	
+
+}
+
+
